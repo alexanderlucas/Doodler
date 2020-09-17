065b27d7dc06eb6d79a6f0353d13d6397e19575e
Alex Lucas <alucas96@me.com>
2020-09-17 07:23:49 -0400

    erasing lines works

--- Doodler/DrawingView.swift
+++ Doodler/DrawingView.swift
@@ -9,38 +9,72 @@ import UIKit
 
 class DrawingView: UIView {
     
-    var drawingLayer = CAShapeLayer()
+    var layers = [CAShapeLayer]()
+    
     let path = UIBezierPath()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        drawingLayer.fillColor = nil
-        drawingLayer.strokeColor = UIColor.label.cgColor
-        
-        layer.addSublayer(drawingLayer)
 
     }
     
     func drawStarted(at point: CGPoint) {
+        let drawingLayer = CAShapeLayer()
+        
+        drawingLayer.fillColor = nil
+        drawingLayer.strokeColor = UIColor.label.cgColor
+        let thickness = Int.random(in: 0..<10)
+        drawingLayer.lineWidth = CGFloat(thickness)
+
+        layer.addSublayer(drawingLayer)
+        layers.append(drawingLayer)
+
         path.move(to: point)
+        
     }
     
     func drawMoved(to point: CGPoint) {
+        guard let drawingLayer = layers.last else {
+            return
+        }
         path.addLine(to: point)
         drawingLayer.path = path.cgPath
     }
     
     func drawEnded() {
+        path.removeAllPoints()
+    }
+    
+    func eraseStarted(at point: CGPoint) {
+        for (i, l) in layers.enumerated() {
+            if l.path!.contains(point) {
+                l.removeFromSuperlayer()
+                layers.remove(at: i)
+            }
+        }
+    }
+    
+    func eraseMoved(to point: CGPoint) {
+        for (i, l) in layers.enumerated() {
+            if l.path!.contains(point) {
+                l.removeFromSuperlayer()
+                layers.remove(at: i)
+            }
+        }
+    }
+    
+    func eraseEnded() {
         
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        
     }
     
     
-    
+
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
--- Doodler/EditDrawingViewController.swift
+++ Doodler/EditDrawingViewController.swift
@@ -9,40 +9,48 @@ import UIKit
 
 class EditDrawingViewController: UIViewController {
     
-    var drawingView: DrawingView?
-
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    @IBOutlet weak var drawingView: DrawingView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action:#selector(handleScreenPan))
 
-        view.addGestureRecognizer(panGestureRecognizer)
-
+        drawingView.addGestureRecognizer(panGestureRecognizer)
         
-        drawingView = DrawingView(frame: self.view.frame)
-        self.view.addSubview(drawingView!)
-
     }
     
     @objc func handleScreenPan(_ sender: UIGestureRecognizer) {
-        guard let drawingView = drawingView else {
-            return
-        }
         
         switch sender.state {
         case .began:
-            let firstPoint = sender.location(in: self.view)
+            let firstPoint = sender.location(in: self.drawingView)
             
-            drawingView.drawStarted(at: firstPoint)
+            if segment.selectedSegmentIndex == 0 {
+                drawingView.drawStarted(at: firstPoint)
+            } else {
+                drawingView.eraseStarted(at: firstPoint)
+            }
             
         case .changed:
-            let point = sender.location(in: self.view)
+            let point = sender.location(in: self.drawingView)
 
-            drawingView.drawMoved(to: point)
+            if segment.selectedSegmentIndex == 0 {
+                drawingView.drawMoved(to: point)
+            } else {
+                drawingView.eraseMoved(to: point)
+            }
 
         case .ended:
-            drawingView.drawEnded()
         
+            if segment.selectedSegmentIndex == 0 {
+                drawingView.drawEnded()
+            } else {
+                drawingView.eraseEnded()
+            }
             
         default: ()
         }
